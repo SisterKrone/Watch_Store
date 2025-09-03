5e8fd523cf64eb18d41c40183fb72bdd75a55d11
SisterKrone <luiz.fabiana12@gmail.com>
2025-09-02 21:22:24 -0300

    Finished

--- scripts.js
+++ scripts.js
@@ -4,23 +4,36 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list .item')
 
 let active = 0
 let firstPosition = 0
 let lastPosition = items.length - 1
 
-nextButton.onclick = () => {
+function setSlider() {
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
 
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = active + 1
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
     items[active].classList.add('active')
+
+
 }
 
 prevButton.onclick = () => {
-    let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
-
-    active = active + 1 > lastPosition ? 0 : active + 1
+    list.style.setProperty('--calculation', -1)
+    active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
     items[active].classList.add('active')
+
 }
